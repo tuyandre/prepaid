c7c2d83cc0910fd790afc7b75b592903a55794c6
tuyandre <tuyandre@gmail.com>
2020-08-16 14:00:36 +0200

    panel css

--- resources/views/shared/master.blade.php
+++ resources/views/shared/master.blade.php
@@ -7,14 +7,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="HandheldFriendly" content="true">
     <title>Prepaid</title>
-    <link rel="icon" href="{{ asset('data/assets/logo.jpg') }}">
-     {{-- <link rel="icon" href="https://storistore.herokuapp.com/clients/assets/icon2.png"> --}}
+ 
+    <link rel="icon" href="https://prepaidproject.herokuapp.com/data/assets/logo.jpg">
+    <link href="https://prepaidproject.herokuapp.com/css/app.css" rel="stylesheet">
+    <link href="https://prepaidproject.herokuapp.com/css/adminHeader.css" rel="stylesheet">
+    <link href="https://prepaidproject.herokuapp.com/css/responsive.css" rel="stylesheet">
+    <link href="https://prepaidproject.herokuapp.com/vendors/flaticon/flaticon.css" rel="stylesheet">
+    <link href="https://prepaidproject.herokuapp.com/vendors/linericon/style.css" rel="stylesheet">
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{--  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/adminHeader.css') }}" rel="stylesheet">
     <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('vendors/flaticon/flaticon.css')}}">
-    <link rel="stylesheet" href="{{ asset('vendors/linericon/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('vendors/linericon/style.css')}}">  --}}
 
     <meta name="csrf-token" content="{{ csrf_token() }}"> </head>
     <body class="hold-transition sidebar-mini layout-fixed">
@@ -34,7 +39,8 @@
     </div>
     <!-- built files will be auto injected -->
     <!-- ./wrapper -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-     {{-- <script src="https://storistore.herokuapp.com/js/app.js" defer></script> --}}
+    {{--  <script src="{{ asset('js/app.js') }}" defer></script>  --}}
+    
+<script src="https://prepaidproject.herokuapp.com/js/app.js" defer></script>
   </body>
 </html>
